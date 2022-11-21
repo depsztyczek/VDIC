04d1ab9116ddb527a0c4ca641c21ff2f4f1860ad
depsztyczek <wojtek.k1999@gmail.com>
2022-11-21 21:35:07 +0100

    Added files which didn't require many changes

---
+++ lab04part2/tb/alu_bfm.sv
@@ -0,0 +1,156 @@
+/*
+ Copyright 2013 Ray Salemi
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+interface alu_bfm;
+	import alu_pkg::*;
+
+	bit                  clk;
+	bit                  rst_n;
+	bit                  enable_n;
+	bit                  din;
+	logic                dout;
+	bit                  dout_valid;
+
+
+	bit           [7:0] A, B;
+	bit           [7:0] status;
+	bit           [7:0] data_msb;
+	bit           [7:0] data_lsb;
+	bit           [15:0] data_result;
+	wire          [7:0]  op;
+
+	test_progress_t      test_progress = TEST_IN_PROGRESS;
+	operation_t          op_set;
+
+	assign op = op_set;
+	assign data_result = {data_msb, data_lsb};
+
+
+
+	modport tlm (import reset_alu, serializer, deserializer, calculate_parity);
+
+//------------------------------------------------------------------------------
+// clock generator
+//------------------------------------------------------------------------------
+	initial begin : clk_gen_blk
+		clk = 0;
+		forever begin : clk_frv_blk
+			#10;
+			clk = ~clk;
+		end
+	end
+
+//------------------------------------------------------------------------------
+// reset task
+//------------------------------------------------------------------------------
+
+	task reset_alu();
+`ifdef DEBUG
+		$display("%0t DEBUG: reset_alu", $time);
+`endif
+		enable_n = 1'b1;
+		rst_n = 1'b0;
+		@(negedge clk);
+		rst_n = 1'b1;
+	endtask : reset_alu
+
+//------------------------------------------------------------------------------
+// serializer task
+//------------------------------------------------------------------------------
+
+	task serializer(input bit [7:0] data, payload_type_t payload_bit);
+
+		bit [9:0] word;
+		static bit parity_bit = 0;
+
+		assign word = {payload_bit, data, parity_bit};
+
+		parity_bit = calculate_parity(word);
+
+		for (int i = 0 ; i < 10 ; i++)
+		begin
+			@(negedge clk);
+			din = word[9-i];
+			enable_n  = 1'b0;
+		end
+
+	endtask
+
+//------------------------------------------------------------------------------
+// deserializer task
+//------------------------------------------------------------------------------
+
+	task deserializer();
+
+		bit [9:0] status_word, data_msb_word, data_lsb_word;
+
+		assign status = status_word[8:1];
+		assign data_msb = data_msb_word[8:1];
+		assign data_lsb = data_lsb_word[8:1];
+
+		wait(dout_valid);
+
+		for (int i = 0 ; i < 10 ; i++)
+		begin
+			@(negedge clk);
+			status_word[9-i] = dout;
+		end
+		assert(calculate_parity(status_word) == status_word[0]);
+
+
+		for (int i = 0 ; i < 10 ; i++)
+		begin
+			@(negedge clk);
+			data_msb_word[9-i] = dout;
+		end
+		assert(calculate_parity(data_msb_word) == data_msb_word[0]);
+
+		for (int i = 0 ; i < 10 ; i++)
+		begin
+			@(negedge clk);
+			data_lsb_word[9-i] = dout;
+		end
+		assert(calculate_parity(data_lsb_word) == data_lsb_word[0]);
+
+
+		begin
+		`ifdef DEBUG
+			$display("Deserializer received:");
+			$display("Status: %b", status_word);
+			$display("Data MSB: %b", data_msb_word);
+			$display("Data LSB: %b", data_lsb_word);
+			$display("Data is %h", data_result);
+		`endif
+		end
+
+	endtask
+
+	function bit calculate_parity(bit [9:0] word);
+		automatic bit parity_bit = 0;
+		for (int i = 1 ; i < 10 ; i++)
+		begin
+			if (word[i] == 1)
+				parity_bit = !parity_bit;
+		end
+
+		return parity_bit;
+
+	endfunction : calculate_parity
+
+
+
+endinterface : alu_bfm
+
+
---
+++ lab04part2/tb/alu_pkg.sv
@@ -0,0 +1,53 @@
+/*
+   Copyright 2013 Ray Salemi
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+`timescale 1ns/1ps
+package alu_pkg;
+	
+	typedef enum bit[7:0] {
+		S_NO_ERROR = 8'b00000000,
+		S_MISSING_DATA = 8'b00000001,
+		S_DATA_STACK_OVERFLOW = 8'b00000010,
+		S_OUTPUT_FIFO_OVERFLOW = 8'b00000100,
+		S_DATA_PARITY_ERROR = 8'b00100000,
+		S_COMMAND_PARITY_ERROR = 8'b01000000,
+		S_INVALID_COMMAND = 8'b10000000
+	} status_t;
+
+	typedef enum bit[7:0] {
+		CMD_NOP = 8'b00000000,
+		CMD_AND = 8'b00000001,
+		CMD_OR = 8'b00000010,
+		CMD_XOR = 8'b00000011,
+		CMD_ADD = 8'b00010000,
+		CMD_SUB = 8'b00100000
+	} operation_t;
+
+	typedef enum bit {
+		TEST_DONE,
+		TEST_IN_PROGRESS
+	} test_progress_t;
+
+	typedef enum bit {
+		CONTROL = 1'b1,
+		DATA = 1'b0
+	} payload_type_t;
+	
+`include "coverage.svh"
+`include "tester.svh"
+`include "scoreboard.svh"
+`include "testbench.svh"
+endpackage : tinyalu_pkg
+   
