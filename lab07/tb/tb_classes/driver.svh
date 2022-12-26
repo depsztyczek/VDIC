1dcf08267aa6af97f5018739a962fe09ba0eff43
depsztyczek <wojtek.k1999@gmail.com>
2022-12-26 20:15:11 +0100

    Update driver to use command transaction

--- lab07/tb/tb_classes/driver.svh
+++ lab07/tb/tb_classes/driver.svh
@@ -15,18 +15,16 @@
  */
 class driver extends uvm_component;
     `uvm_component_utils(driver)
-
+    
 //------------------------------------------------------------------------------
 // local variables
 //------------------------------------------------------------------------------
-
-    protected virtual tinyalu_bfm bfm;
+    protected virtual alu_bfm bfm;
     uvm_get_port #(command_transaction) command_port;
-
+    
 //------------------------------------------------------------------------------
 // constructor
 //------------------------------------------------------------------------------
-
     function new (string name, uvm_component parent);
         super.new(name, parent);
     endfunction : new
@@ -34,28 +32,23 @@ class driver extends uvm_component;
 //------------------------------------------------------------------------------
 // build phase
 //------------------------------------------------------------------------------
-
     function void build_phase(uvm_phase phase);
-        if(!uvm_config_db #(virtual tinyalu_bfm)::get(null, "*","bfm", bfm))
-            `uvm_fatal("DRIVER", "Failed to get BFM")
+        if(!uvm_config_db #(virtual alu_bfm)::get(null, "*","bfm", bfm))
+            $fatal(1, "Failed to get BFM");
         command_port = new("command_port",this);
     endfunction : build_phase
-
+    
 //------------------------------------------------------------------------------
-// run_phase
+// run phase
 //------------------------------------------------------------------------------
-
     task run_phase(uvm_phase phase);
-        byte unsigned iA;
-        byte unsigned iB;
-        operation_t op_set;
-        shortint result;
         command_transaction command;
+
         forever begin : command_loop
             command_port.get(command);
-            bfm.send_op(command.A, command.B, command.op, result);
+            bfm.send_op(command.A, command.B, command.op);
         end : command_loop
     endtask : run_phase
 
-
 endclass : driver
+
