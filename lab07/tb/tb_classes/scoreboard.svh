811ef4d69e4e64d0231aac5d6de8041c1b274a7e
depsztyczek <wojtek.k1999@gmail.com>
2022-12-26 20:56:56 +0100

    Updated scoreboard (double check that)

--- lab07/tb/tb_classes/scoreboard.svh
+++ lab07/tb/tb_classes/scoreboard.svh
@@ -17,95 +17,127 @@ class scoreboard extends uvm_subscriber #(result_transaction);
     `uvm_component_utils(scoreboard)
 
 //------------------------------------------------------------------------------
-// local typedefs
-//------------------------------------------------------------------------------
-
-    typedef enum bit {
-        TEST_PASSED,
-        TEST_FAILED
-    } test_result;
-
-//------------------------------------------------------------------------------
 // local variables
 //------------------------------------------------------------------------------
-
     uvm_tlm_analysis_fifo #(command_transaction) cmd_f;
-
-    local test_result tr = TEST_PASSED; // the result of the current test
+    protected test_result_t test_result = TEST_PASSED;
 
 //------------------------------------------------------------------------------
 // constructor
 //------------------------------------------------------------------------------
-
     function new (string name, uvm_component parent);
         super.new(name, parent);
     endfunction : new
 
 //------------------------------------------------------------------------------
-// print the PASSED/FAILED in color
+// function to calculate the expected ALU result
 //------------------------------------------------------------------------------
-    local function void print_test_result (test_result r);
-        if(tr == TEST_PASSED) begin
-            set_print_color(COLOR_BOLD_BLACK_ON_GREEN);
-            $write ("-----------------------------------\n");
-            $write ("----------- Test PASSED -----------\n");
-            $write ("-----------------------------------");
-            set_print_color(COLOR_DEFAULT);
-            $write ("\n");
-        end
-        else begin
-            set_print_color(COLOR_BOLD_BLACK_ON_RED);
-            $write ("-----------------------------------\n");
-            $write ("----------- Test FAILED -----------\n");
-            $write ("-----------------------------------");
-            set_print_color(COLOR_DEFAULT);
-            $write ("\n");
-        end
-    endfunction
+	function logic [15:0] get_expected_data(
+			bit [7:0] A,
+			bit [7:0] B,
+			operation_t op_set
+		);
+
+		bit [15:0] ret;
+
+		case(op_set)
+			CMD_AND : ret    = A & B;
+			CMD_ADD : ret    = A + B;
+			CMD_XOR : ret    = A ^ B;
+			CMD_NOP : ret    = 16'h0000;
+			CMD_OR  : ret    = A | B;
+			CMD_SUB : ret    = A - B;
+			default: begin
+				ret = 16'h0000;
+			end
+		endcase
+
+		return(ret);
+
+	endfunction : get_expected_data
+
+	function logic [7:0] get_expected_status(
+			operation_t op_set
+		);
+
+		bit [7:0] ret;
+
+		case(op_set)
+			CMD_AND : ret    = S_NO_ERROR;
+			CMD_ADD : ret    = S_NO_ERROR;
+			CMD_XOR : ret    = S_NO_ERROR;
+			CMD_NOP : ret    = S_NO_ERROR;
+			CMD_OR  : ret    = S_NO_ERROR;
+			CMD_SUB : ret    = S_NO_ERROR;
+
+			default: begin
+				ret = S_INVALID_COMMAND;
+			end
+
+		endcase
+
+		return(ret);
+
+	endfunction : get_expected_status
+	
+	function logic [23:0] get_expected_result(command_transaction cmd);
+		bit [23:0] ret;
+		bit [7:0] status;
+		bit [15:0] data;
+		
+		status = get_expected_status(cmd.op);
+		data = get_expected_data(cmd.A, cmd.B, cmd.op);
+		ret = {status, data};
+		
+		return(ret);
+
+	endfunction : get_expected_result
+	
+	protected function void print_test_result (test_result_t r);
+		if(r == TEST_PASSED) begin
+			set_print_color(COLOR_BOLD_BLACK_ON_GREEN);
+			$write ("-----------------------------------\n");
+			$write ("----------- Test PASSED -----------\n");
+			$write ("-----------------------------------");
+			set_print_color(COLOR_DEFAULT);
+			$write ("\n");
+		end
+		else begin
+			set_print_color(COLOR_BOLD_BLACK_ON_RED);
+			$write ("-----------------------------------\n");
+			$write ("----------- Test FAILED -----------\n");
+			$write ("-----------------------------------");
+			set_print_color(COLOR_DEFAULT);
+			$write ("\n");
+		end
+	endfunction
 
 //------------------------------------------------------------------------------
 // build phase
 //------------------------------------------------------------------------------
-
     function void build_phase(uvm_phase phase);
         cmd_f = new ("cmd_f", this);
     endfunction : build_phase
 
-//------------------------------------------------------------------------------
-// function to calculate the expected ALU result
-//------------------------------------------------------------------------------
-
-    local function result_transaction predict_result(command_transaction cmd);
-        result_transaction predicted;
-
-        predicted = new("predicted");
-
-        case (cmd.op)
-            add_op: predicted.result = cmd.A + cmd.B;
-            and_op: predicted.result = cmd.A & cmd.B;
-            xor_op: predicted.result = cmd.A ^ cmd.B;
-            mul_op: predicted.result = cmd.A * cmd.B;
-        endcase // case (op_set)
-
-        return predicted;
-
-    endfunction : predict_result
 
 //------------------------------------------------------------------------------
 // subscriber write function
 //------------------------------------------------------------------------------
-
     function void write(result_transaction t);
-        string data_str;
+	    
+	    string data_str;
+		result_transaction predicted_result;
         command_transaction cmd;
-        result_transaction predicted;
+	    
+        cmd.A            = 0;
+        cmd.B            = 0;
+        cmd.op           = CMD_NOP;
 
         do
             if (!cmd_f.try_get(cmd))
                 $fatal(1, "Missing command in self checker");
-        while ((cmd.op == no_op) || (cmd.op == rst_op));
-
-        predicted = predict_result(cmd);
+        while (cmd.op == CMD_NOP);
+        predicted_result = get_expected_result(cmd.A, cmd.B, cmd.op);
 
         data_str  = { cmd.convert2string(),
             " ==>  Actual " , t.convert2string(),
@@ -117,18 +149,21 @@ class scoreboard extends uvm_subscriber #(result_transaction);
         end
         else
             `uvm_info ("SELF CHECKER", {"PASS: ", data_str}, UVM_HIGH)
-
+            
     endfunction : write
 
 //------------------------------------------------------------------------------
 // report phase
 //------------------------------------------------------------------------------
-
     function void report_phase(uvm_phase phase);
         super.report_phase(phase);
-        print_test_result(tr);
+        print_test_result(test_result);
     endfunction : report_phase
 
-
-
 endclass : scoreboard
+
+
+
+
+
+
