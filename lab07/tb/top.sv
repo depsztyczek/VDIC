ca948dc9de00dea7b46c89c7b05be8b820159371
depsztyczek <wojtek.k1999@gmail.com>
2022-12-26 21:07:57 +0100

    Updated top.sv

--- lab07/tb/top.sv
+++ lab07/tb/top.sv
@@ -1,36 +1,33 @@
 /*
-   Copyright 2013 Ray Salemi
+ Copyright 2013 Ray Salemi
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 module top;
-   import uvm_pkg::*;
-   import   tinyalu_pkg::*;
-`include "tinyalu_macros.svh"
+import uvm_pkg::*;
 `include "uvm_macros.svh"
-   
-   tinyalu_bfm       bfm();
-   tinyalu DUT (.A(bfm.A), .B(bfm.B), .op(bfm.op), 
-                .clk(bfm.clk), .reset_n(bfm.reset_n), 
-                .start(bfm.start), .done(bfm.done), .result(bfm.result));
+import alu_pkg::*;
 
+	
+alu_bfm bfm();
+vdic_dut_2022 DUT (.clk(bfm.clk), .rst_n(bfm.rst_n), .enable_n(bfm.enable_n), .din(bfm.din), .dout(bfm.dout), .dout_valid(bfm.dout_valid));
 
 initial begin
-   uvm_config_db #(virtual tinyalu_bfm)::set(null, "*", "bfm", bfm);
-   run_test();
+    uvm_config_db #(virtual alu_bfm)::set(null, "*", "bfm", bfm);
+    run_test();
 end
 
 endmodule : top
 
-     
-   
+
+
